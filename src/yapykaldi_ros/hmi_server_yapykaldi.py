068fa9aef7396f21361483864cd67ef37f27ab48
Loy van Beek <loy.vanbeek@gmail.com>
2020-04-07 21:30:57 +0200

    Set up ASsr instance based on rosparams

--- src/yapykaldi_ros/hmi_server_yapykaldi.py
+++ src/yapykaldi_ros/hmi_server_yapykaldi.py
@@ -1,14 +1,18 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
-from builtins import *
-import rospy
-from yapykaldi import Asr
 from hmi import AbstractHMIServer, HMIResult
+import os
+import rospy
+from yapykaldi.asr import Asr
 
 
 class HMIServerYapykaldi(AbstractHMIServer):
     """HMI server wrapper yapykaldi ASR app"""
     def __init__(self):
-        super().__init__(rospy.get_name())
+        super(HMIServerYapykaldi, self).__init__(rospy.get_name())
+
+        self._asr = Asr(model_dir=os.path.expanduser(rospy.get_param('~model_dir')),
+                        model_type=rospy.get_param('~model_type', 'nnet3'),
+                        output_dir=rospy.get_param('~output_dir', '/tmp/hmi_server_yapykaldi'))
 
     def _determine_answer(self, description, grammar, target, is_preempt_requested):
         """
