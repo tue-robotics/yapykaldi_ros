d06eb4923e5e4628e43b71e173c9e5427f972739
Loy van Beek <loy.vanbeek@gmail.com>
2020-04-13 20:07:08 +0200

    Call recognize correctly and get actual results back
    WIP: reading a file stream instead of live audio

--- src/yapykaldi_ros/hmi_server_yapykaldi.py
+++ src/yapykaldi_ros/hmi_server_yapykaldi.py
@@ -1,33 +1,44 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from hmi import AbstractHMIServer, HMIResult
-from hmi.common import parse_sentence
+from hmi.common import parse_sentence, verify_grammar
 import os
 import rospy
 from std_msgs.msg import String
-from yapykaldi.asr import Asr
+from yapykaldi.asr import Asr, WaveFileStreamer
 
 
 class HMIServerYapykaldi(AbstractHMIServer):
     """HMI server wrapper yapykaldi ASR app"""
     def __init__(self):
-        super(HMIServerYapykaldi, self).__init__(rospy.get_name())
-
+        stream = WaveFileStreamer("/home/loy/output.wav")
+        rospy.loginfo("Setting up ASR, may take a while...")
         self._asr = Asr(model_dir=os.path.expanduser(rospy.get_param('~model_dir')),
                         model_type=rospy.get_param('~model_type', 'nnet3'),
-                        output_dir=rospy.get_param('~output_dir', '/tmp/hmi_server_yapykaldi'))
+                        stream=stream)
+        rospy.loginfo("Set up ASR")
 
-        self._asr.register_callback(self.string_recognized_callback)
+        self._asr.register_fully_recognized_callback(self.string_fully_recognized_callback)
+        self._asr.register_partially_recognized_callback(self.string_partially_recognized_callback)
 
         self._string = ""
+
+        super(HMIServerYapykaldi, self).__init__(rospy.get_name())
 
     def stop(self):
         rospy.loginfo("Stopping {}".format(self))
         self._asr.stop()
 
-    def string_recognized_callback(self, string):
-        self._string = string  # Using a threading primitive for this would be nicer!
+    def string_fully_recognized_callback(self, string):
+        # type: (str) -> None
+        rospy.loginfo("Got a string: '{}'".format(string))
+        self._string = string.strip()  # Using a threading primitive for this would be nicer!
+
+    def string_partially_recognized_callback(self, string):
+        # type: (str) -> None
+        rospy.loginfo("Got an intermediate result string: '{}'".format(string))
 
     def _determine_answer(self, description, grammar, target, is_preempt_requested):
+        # type: (str, str, str, Func) -> None
         """
         Method override to start speech recognition upon receiving a query
         from the HMI server
@@ -37,10 +48,15 @@ class HMIServerYapykaldi(AbstractHMIServer):
         :param target: (str) target that should be obtained from the grammar
         :param is_preempt_requested: (callable) checks whether a preempt is requested by the hmi client
         """
-        rospy.loginfo("Need an answer from ASR!")
+        rospy.loginfo("Need an answer from ASR for '{}'".format(description))
         self._string = None
+
+        verify_grammar(grammar)
+
+        self._asr.start()
+
         while not rospy.is_shutdown() and not is_preempt_requested() and not self._string:
-            rospy.loginfo("Sleep")
+            rospy.logdebug("Sleep")
             rospy.sleep(.1)
 
         if rospy.is_shutdown() or is_preempt_requested():
